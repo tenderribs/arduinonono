06eb776aa635c9d36b17586b0a9c7bd3aea1a72d
tenderribs <31791898+tenderribs@users.noreply.github.com>
2018-01-05 10:52:49 +0100

    Add files via upload

---
+++ servo_simple.ino
@@ -0,0 +1,26 @@
+#include <Servo.h> 
+int servoPin = 2;
+Servo Servo1;
+
+void setup() {
+  Servo1.attach(servoPin);
+  
+      Servo1.write(0);
+      delay(100);  
+   
+      Servo1.write(45);
+      delay(100);
+
+      Servo1.write(90);
+      delay(100);
+
+            Servo1.write(135);
+      delay(100);  
+   
+      Servo1.write(175);
+      delay(100);
+}
+
+void loop(){
+}
+
